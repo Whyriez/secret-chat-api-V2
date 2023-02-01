0d961fd99a66184a3204cb7c0094e9282efec4c7
alimsx <81366596+Whyriez@users.noreply.github.com>
2023-02-01 18:42:18 +0800

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,29 +1,30 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-  "freedb_secret-chat",
-  "freedb_secret-user",
-  "AWpVpFE?VB82qSc",
-  {
-     host: "sql.freedb.tech",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
-
 // const db = new Sequelize(
-//   "secret-chat",
-//   "a9627n062go85cg42n92",
-//   "pscale_pw_LIgY5s7TezAAbfmqQRHX59OwrCgdEDy56fNTD9LPS6C",
+//   "freedb_secret-chat",
+//   "freedb_secret-user",
+//   "AWpVpFE?VB82qSc",
 //   {
-//     host: "ap-southeast.connect.psdb.cloud",
+//      host: "sql.freedb.tech",
 //     dialect: "mysql",
-//     dialectOptions: {
-//       ssl: {
-//         rejectUnauthorized: true,
-//       },
-//     },
+//     port: 3306,
 //   }
 // );
+
+const db = new Sequelize(
+  "secret-chat",
+  "a9627n062go85cg42n92",
+  "pscale_pw_LIgY5s7TezAAbfmqQRHX59OwrCgdEDy56fNTD9LPS6C",
+  {
+    host: "ap-southeast.connect.psdb.cloud",
+    dialect: "mysql",
+    port: 3306,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    },
+  }
+);
 
 export default db;
