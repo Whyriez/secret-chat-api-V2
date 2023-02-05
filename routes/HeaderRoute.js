047ee47c72a270c2b610855caa48aee78a7b5f63
alimsx <81366596+Whyriez@users.noreply.github.com>
2023-02-05 17:52:14 +0800

    Update HeaderRoute.js

--- routes/HeaderRoute.js
+++ routes/HeaderRoute.js
@@ -9,14 +9,6 @@ import {
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-res.setHeader("Access-Control-Allow-Origin", "*")
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Max-Age", "1800");
-res.setHeader("Access-Control-Allow-Headers", "content-type");
-res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
- });
-
 router.get("/user_header", getHeader);
 router.get("/user_header/:id", getHeaderById);
 router.post("/user_header", createHeader);
